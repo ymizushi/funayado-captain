097740f3dbfddf050c143eb3fa11fc00fe477d8f
ymizushi <mizushi@gmail.com>
2022-11-23 18:17:51 +0900

    add coverage constraint

--- jest.config.ts
+++ jest.config.ts
@@ -18,6 +18,14 @@ const customJestConfig = {
   moduleNameMapper: {
     "^@/(.*)": "<rootDir>/src/$1",
   },
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90, 
+    }
+  }
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
