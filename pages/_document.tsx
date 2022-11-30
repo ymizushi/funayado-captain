c50ab7c0976211bcce45e5805fb0dd0e240b5dfe
ymizushi <mizushi@gmail.com>
2022-12-01 00:11:19 +0900

    to pwa application

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <meta name="theme-color" content="#fff" />
         <link
           href="https://fonts.googleapis.com/earlyaccess/nicomoji.css"
           rel="stylesheet"
